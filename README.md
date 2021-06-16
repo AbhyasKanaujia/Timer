c31c92c847ea80d4ee8e9c871d23f5784369643a
AbhyasKanaujia <44172451+AbhyasKanaujia@users.noreply.github.com>
2021-06-16 13:32:59 +0530

    Add Timer Functionality

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Timer
+
 Timer app I made for myself because others didn't do what I needed/wanted.
 
 https://abhyaskanaujia.github.io/Timer/
+
+## Features
+
+- Fully Responsive
---
+++ scripts/timer.js
@@ -0,0 +1,67 @@
+var timerRunning = false;
+var initialState = true;
+var beginTime;
+var timerDisplay;
+var difference;
+
+function toggleTimer() {
+    timerRunning = !timerRunning;
+    initializeTimer();
+    if (timerRunning) timerCycle();
+    updateUIToggle();
+
+}
+
+function initializeTimer() {
+    if (initialState) {
+        beginTime = moment();
+        initialState = false;
+    } else {
+        beginTime = moment().subtract(difference);
+    }
+}
+
+function timerCycle() {
+    if (timerRunning) {
+        var now = moment();
+        difference = moment.duration(now.diff(beginTime));
+
+        updateUINewTime(difference.as('hours'), difference.as('minutes'), difference.as('seconds'), difference.as('milliseconds'));
+        setTimeout("timerCycle()", 100);
+    }
+}
+
+function updateUINewTime(hour, minute, second, millisecond) {
+    hour = parseInt(hour);
+    minute = parseInt(minute);
+    second = parseInt(second);
+    millisecond = parseInt(millisecond % 10);
+
+    if (hour < 10) hour = '0' + hour;
+    if (minute < 10) minute = '0' + minute;
+    if (second < 10) second = '0' + second;
+
+    $('#hourDisplay').text(hour);
+    $('#minuteDisplay').text(minute);
+    $('#secondDisplay').text(second);
+    $('#millisecondDisplay').text(millisecond);
+}
+
+
+function updateUIToggle() {
+    if (!initialState) {
+        $('#stopButton').prop('disabled', false);
+        $('#flagButton').prop('disabled', false);
+    }
+    if (timerRunning) {
+        $('#toggleButtonIcon').removeClass('fa-play');
+        $('#toggleButtonIcon').addClass('fa-pause');
+        $('#stopButton').prop('disabled', false);
+        $('#flagButton').prop('disabled', false);
+    } else {
+        $('#toggleButtonIcon').removeClass('fa-pause');
+        $('#toggleButtonIcon').addClass('fa-play');
+    }
+
+}
+
